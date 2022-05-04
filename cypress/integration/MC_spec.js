fa8b3e57ab44ebb5419a9bcd219acf0c5410edf2
Anjana Priya <79685574+AnjanaPriya@users.noreply.github.com>
2022-05-04 11:09:52 +0530

    Add files via upload

---
+++ cypress/integration/MC_spec.js
@@ -0,0 +1,23 @@
+describe('Merchant COnsole Automation',function(){
+    
+    Cypress.on('uncaught:exception', (err, runnable) => {
+        // returning false here prevents Cypress from
+        // failing the test
+        return false
+    })
+
+    it('installing the wishlist app',function(){
+
+        cy.visit('https://admin.swymrelay.com/swym-login')
+    
+        cy.get('#username').type('test-login1@gmail.com')
+    
+        cy.get('#password').type('UniversityBank6^')
+        
+        cy.wait(4000)
+
+        cy.get('button').contains("Login").click()
+        
+    })
+
+})
